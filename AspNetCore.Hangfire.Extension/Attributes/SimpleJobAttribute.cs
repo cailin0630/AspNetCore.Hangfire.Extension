fd4a0b6b11bbc2547a643f6481b7d2f8e98ff533
江毅 <903199067@qq.com>
2020-02-29 17:47:04 +0800

    update SimpleJobAttribute

--- AspNetCore.Hangfire.Extension/Attributes/SimpleJobAttribute.cs
+++ AspNetCore.Hangfire.Extension/Attributes/SimpleJobAttribute.cs
@@ -9,9 +9,25 @@ namespace AspNetCore.Hangfire.Extension.Attributes
     public class SimpleJobAttribute : Attribute
     {
         /// <summary>
-        /// Cron表达式
+        /// 无参构造
         /// </summary>
-        public string CronExpression { get; set; }
+        public SimpleJobAttribute()
+        {
+
+        }
+
+        /// <summary>
+        /// 带参数构造
+        /// </summary>
+        /// <param name="isOpen"></param>
+        /// <param name="jobId"></param>
+        /// <param name="cronExpression"></param>
+        public SimpleJobAttribute(bool isOpen, string jobId, string cronExpression)
+        {
+            IsOpen = isOpen;
+            JobId = jobId;
+            CronExpression = cronExpression;
+        }
 
         /// <summary>
         /// 是否开启
@@ -22,5 +38,10 @@ namespace AspNetCore.Hangfire.Extension.Attributes
         /// job唯一标识
         /// </summary>
         public string JobId { get; set; }
+        /// <summary>
+        /// Cron表达式
+        /// </summary>
+
+        public string CronExpression { get; set; }
     }
 }
